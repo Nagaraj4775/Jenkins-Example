a233958205272f5cdf9007b0573cceb2ad3e5c1f
Nagaraj <rapellinagaraj@gmail.com>
2020-07-25 16:46:53 +0530

    Update envVars.groovy

--- envVars.groovy
+++ envVars.groovy
@@ -3,6 +3,7 @@ def qa(){
         env.ANDROID_APP_VERSION = "qa_android-${BUILD_NUMBER}.apk"
         env.ANDROID_PLATFORM = "android"
         env.ANDROID_APK_NAME = "androidtv_${BUILD_ENV}_signed"
+        env.ANDROID_APK_PATH = "apps/release/cnn_go_android/app"
 }    
 def prod(){     
         env.BUILD_ENV = "prod"
